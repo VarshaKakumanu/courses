85b29a9ba65ecff58a5dadb78ceea331010de72d
HONEYCOMB\Varsha <varsha.honeycomb@gmail.com>
2023-03-20 13:16:42 +0530

    added  sidebar

---
+++ src/compponents/Courses.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sidebar from './Sidebar'
+
+function Courses() {
+  return (
+    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+        <Sidebar />
+    </div>
+  )
+}
+
+export default Courses
