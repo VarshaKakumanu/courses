ea79effb1b9c7c86eb7ea6e0db16b66fd0c1d1dc
HONEYCOMB\Varsha <varsha.honeycomb@gmail.com>
2023-03-21 14:47:46 +0530

    added style routing

--- src/Router.jsx
+++ src/Router.jsx
@@ -6,6 +6,10 @@ import Cs from './compponents/courses/Cs';
 import Iot from './compponents/courses/Iot';
 import Deveops from './compponents/courses/Deveops';
 import Networking from './compponents/courses/Networking';
+import Sub1 from './compponents/courses/cs/Sub1';
+import Sub2 from './compponents/courses/cs/Sub2';
+import Sub3 from './compponents/courses/cs/Sub3';
+import Sub4 from './compponents/courses/cs/Sub4';
 
 const Router = () => {
   return (
@@ -16,6 +20,10 @@ const Router = () => {
           <Route path="deveops" element={ <Deveops /> } />
           <Route path="networking" element={ <Networking /> } />
           <Route path="iot" element={ <Iot /> } />
+          <Route path="sub1" element={ <Sub1 /> } />
+          <Route path="sub2" element={ <Sub2 /> } />
+          <Route path="sub3" element={ <Sub3 /> } />
+          <Route path="sub4" element={ <Sub4 /> } />
         </Routes>
         </div>
   )
--- src/compponents/courses/Cs.jsx
+++ src/compponents/courses/Cs.jsx
@@ -4,11 +4,12 @@ import { Link } from "react-router-dom";
 function Cs() {
   return (
     <div>
-      <div className='flex flex-col h-screen items-center justify-center p-4 g-3'>
-        <Link to="" class="bg-pink-500 mb-3 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded">Introduction to Computers</Link>
-        <Link to="" class="bg-pink-500 mb-3 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded">Programming</Link>
-        <Link to="" class="bg-pink-500 mb-3 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded">Windows,Features and Applications</Link>
-        <Link to="" class="bg-pink-500 mb-3 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded">Computer Organization</Link>
+      <div className='flex flex-col p-4 g-3 h-screen items-center justify-center'>
+      <h1 className='box-decoration-clone bg-gradient-to-r from-indigo-600 to-pink-500 text-white p-2 mb-4 font-bold text-2xl rounded'>Computer Science</h1>
+        <Link to="sub1" class="bg-pink-500 mb-3 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded">Introduction to Computers</Link>
+        <Link to="sub2" class="bg-pink-500 mb-3 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded">Programming</Link>
+        <Link to="sub3" class="bg-pink-500 mb-3 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded">Windows,Features and Applications</Link>
+        <Link to="sub4" class="bg-pink-500 mb-3 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded">Computer Organization</Link>
       </div>
     </div>
   )
